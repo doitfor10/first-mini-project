a976d44786a42f8d20034e4733f4e9d279577059
youngeun <summer_97@naver.com>
2021-03-01 16:07:27 +0900

    plus pymongo

--- miniproject/app.py
+++ miniproject/app.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+
 app = Flask(__name__)
+
+# client = MongoClient('mongodb://test:test@localhost', 27017) 서버 올릴 때 쓰기
+client = MongoClient('localhost', 27017)
+db = client.dbrecipe
 
 @app.route('/')
 def home():
